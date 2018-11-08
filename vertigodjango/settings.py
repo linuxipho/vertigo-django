f63c3e19c1b047a2cde18e0acc1222b12d608a9d
Rémi de Chazelles <remidechazelles@gmail.com>
2018-11-08 13:40:31 +0100

    Set STATIC_ROOT according to DEBUG mode

--- vertigodjango/settings.py
+++ vertigodjango/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = config('STATIC_ROOT', 'static')  # prod -> os.path.join(BASE_DIR, "../www/static")
+if not DEBUG:
+    STATIC_ROOT = config('STATIC_ROOT', 'static')  # prod -> os.path.join(BASE_DIR, "../www/static")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
